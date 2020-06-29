3ec75762748d18ac718c9d220f4aafdb303a69ae
afeiship <1290657123@qq.com>
2020-06-29 11:25:08 +0800

    feat: udpate get unit case

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -5,9 +5,9 @@
   const http = NxFetch.getInstance({ fetch: nodeFetch });
 
   describe('NxFetch.methods', function () {
-    test('get should return status: 200', function (done) {
-      http.get('http://t.weather.sojson.com/api/weather/city/101030100').then(res => {
-        expect(res.status).toBe(200);
+    test('get should return res.login === afeiship', function (done) {
+      http.get('https://api.github.com/users/afeiship').then(res => {
+        expect(res.login).toBe('afeiship');
         done();
       })
     });
