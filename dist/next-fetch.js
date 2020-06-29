67425577b07b57f834864d84a38f3bf90301f751
afeiship <1290657123@qq.com>
2020-06-29 10:22:17 +0800

    fix: abstract order

--- dist/next-fetch.js
+++ dist/next-fetch.js
@@ -2,8 +2,8 @@
  * name: @feizheng/next-fetch
  * description: Abstract for browser or node.
  * homepage: https://github.com/afeiship/next-fetch
- * version: 1.3.2
- * date: 2020-06-29T02:14:31.654Z
+ * version: 1.3.3
+ * date: 2020-06-29T02:22:00.260Z
  * license: MIT
  */
 
