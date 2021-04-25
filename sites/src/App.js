9d5879f104ba1c01c93a35e90683d25fd539a979
afeiship <1290657123@qq.com>
2021-04-25 14:38:13 +0800

    feat: add timeout/cancelable with middlewares

--- sites/src/App.js
+++ sites/src/App.js
@@ -12,7 +12,7 @@ const http = NxFetch.getInstance({
     { name: 'res1', type: 'response', fn: res1 },
     { name: 'remove-url-field', type: 'response', fn: removeUrlField },
     { name: 'normalize-date', type: 'response', fn: normalizeDate },
-    { name: 'trim-null', type: 'response', fn: trimNull },
+    { name: 'trim-null', type: 'response', fn: trimNull }
   ]
 });
 
@@ -23,9 +23,17 @@ function App() {
       Hello App
       <button
         onClick={(e) => {
-          http.get('https://api.github.com/users/afeiship', { cancelable: true }).then((res) => {
-            console.log(res);
-          });
+          const res = http
+            .get('https://api.github.com/users/afeiship', { timeout: 100, cancelable: true })
+            .then((res) => {
+              console.log(res);
+            })
+            .catch((err) => {
+              if (err.type === 'timeout') {
+                res.cancel();
+                console.log(err);
+              }
+            });
         }}>
         Get api
       </button>
--- src/index.js
+++ src/index.js
@@ -8,12 +8,14 @@
   var nxDeepAssign = nx.deepAssign || require('@jswork/next-deep-assign');
   var nxParam = nx.param || require('@jswork/next-param');
   var nxToAction = nx.toAction || require('@jswork/next-to-action');
-  var nxCancelableFetch = nx.cancelableFetch || require('@jswork/next-cancelable-fetch');
+  var nxApplyFetchMiddleware = nx.applyFetchMiddleware || require('@jswork/next-apply-fetch-middleware');
+  var nxFetchWithTimeout = nx.fetchWithTimeout || require('@jswork/next-fetch-with-timeout');
+  var nxFetchWithCancelable = nx.fetchWithCancelable || require('@jswork/next-fetch-with-cancelable');
+  var middlewares = [nxFetchWithTimeout, nxFetchWithCancelable];
 
   var DEFAULT_OPTIONS = {
     dataType: 'json',
     responseType: 'json',
-    cancelable: false,
     interceptors: [],
     response: nx.stubValue,
     fetch: global.fetch
@@ -25,6 +27,7 @@
       init: function (inOptions) {
         this.base(inOptions);
         this.interceptor = new NxInterceptor({ items: this.options.interceptors });
+        this.httpRequest = nxApplyFetchMiddleware(middlewares)(this.options.fetch);
       },
       defaults: function () {
         return DEFAULT_OPTIONS;
@@ -32,7 +35,6 @@
       request: function (inMethod, inUrl, inData, inOptions) {
         var self = this;
         var options = nx.mix(null, this.options, inOptions);
-        var httpRequest = options.cancelable ? nxCancelableFetch : options.fetch;
         var isGET = String(inMethod).toLowerCase() === 'get';
         var body = isGET ? null : NxDataTransform[options.dataType](inData);
         var path = isGET ? nxParam(inData, inUrl) : inUrl;
@@ -44,7 +46,8 @@
           : nx.stubValue;
 
         return new Promise(function (resolve, reject) {
-          httpRequest(requestOpts.url, config)
+          self
+            .httpRequest(requestOpts.url, config)
             .then(responseHandler)
             .then(function (response) {
               var params = nx.mix({ data: response }, requestOpts);
