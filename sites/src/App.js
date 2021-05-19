743b1de328f2c7af68afdcc0846ee4ff7022f1ff
afeiship <1290657123@qq.com>
2021-05-19 09:39:36 +0800

    feat: update global request-transofrm

--- sites/src/App.js
+++ sites/src/App.js
@@ -1,11 +1,16 @@
-import NxFetch from '../../dist/index';
+import NxFetch from '../../src/index';
 import res1 from './interceptors/response/res1';
 import removeUrlField from './interceptors/response/remove-url-field';
 import normalizeDate from './interceptors/response/normalize-date';
 import trimNull from './interceptors/response/trim-null';
 
 const http = NxFetch.getInstance({
-  response: ({ url, config, data }) => {
+  transformRequest: ({ url, config }) => {
+    console.log('url requst in global config', url);
+    return { url, config };
+  },
+  transformResponse: ({ url, config, data }) => {
+    console.log(url, config);
     return data;
   },
   interceptors: [
@@ -26,7 +31,31 @@ function App() {
       <button
         onClick={(e) => {
           const res = http
-            .get('https://api.github.com/users/afeiship', { timeout: 10 * 1000, cancelable: true })
+            .get('https://api.github.com/users/afeiship', {
+              timeout: 10 * 1000,
+              cancelable: true,
+              transformRequest: ({ url, config }) => {
+                console.log('url requst intance', url);
+                return { url: url + `?ts=${Date.now()}`, config };
+              }
+            })
+            .then((res) => {
+              console.log(res);
+            })
+            .catch((err) => {
+              if (err.type === 'timeout') {
+                res.cancel();
+                console.log(err);
+              }
+            });
+        }}>
+        Get api
+      </button>
+
+      <button
+        onClick={(e) => {
+          const res = http
+            .get('https://api.github.com/users/afeiship', {})
             .then((res) => {
               console.log(res);
             })
