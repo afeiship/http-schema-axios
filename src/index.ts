2f7964b4652c847c1b6d42050e9b33ab4bec5241
afeiship <1290657123@qq.com>
2022-12-08 21:36:05 +0800

    feat: init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-export default (): void => {
-  console.log('hello');
+import httpSchema from '@jswork/http-schema';
+import NxAxios from '@jswork/next-axios';
+
+export default (inConfig, inOptions?) => {
+
+  const http = NxAxios.getInstance({
+    ...inOptions
+  });
+
+  return httpSchema(inConfig, http);
 };
